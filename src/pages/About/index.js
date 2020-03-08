c01653426164863a45447835d907fc178bfb9f1c
McCabe Northup <mccabenorthup@gmail.com>
2020-03-08 15:32:05 -0600

    updated aboutme blurb

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -10,37 +10,37 @@ const About = () => {
         <>
             <div className="blurb row">
                 <div className="four columns">
-                    <img className="portfolioPic" src={Pic} alt="New Zealand"/>
+                    <img className="portfolioPic" src={Pic} alt="New Zealand" />
                 </div>
-                    <div id="paraEl" className="six columns">
-                        <div className="row">
-                            <h1>About Me</h1>
-                        </div>
-                        <div className="row">
-                            <p>Full Stack Web Developer with a background in customer facing management and refined skills in
-                                HTML, CSS, JavaScript, React, VBA, Node.js, and MySQL from University of Denver. Strengths in
-                                adaptability, communication, and building dynamic projects within a team. Pioneering Problem
-                                solver with a passion for creating well developed projects.</p>
-                            <span className="icon github">
-                                <a target="_blank" href="https://github.com/mnorthup2207">
-                                    <i className="fab fa-github"></i>
-                                </a>
-                            </span>
-                            <span className="icon linked">
-                                <a target="_blank" href="https://www.linkedin.com/in/mccabe-northup-2b76b885/">
-                                    <i className="fab fa-linkedin-in"></i>
-                                </a>
-                            </span>
-                            <span className="icon resume">
-                                <a target="_blank" href={PDF}>
-                                    <i className="fas fa-file-pdf"></i>
-                                </a>
-                            </span>
-                        </div>
+                <div id="paraEl" className="six columns">
+                    <div className="row">
+                        <h1>About Me</h1>
                     </div>
-                </div>  
-            </>
-            );
-        };
-        
+                    <div className="row">
+                        <p>Full Stack Web Developer with refined skills in the MERN Stack. 
+                            MongoDB, ExpressJS, ReactJS, and NodeJS. In addition, I am also skilled in MySQL, Sequelize, and Command Line. 
+                            I have a wealth of knowledge in version control and collaboration through GitHub. 
+                            I have 30+ repositories with more than 500 commits.</p>
+                        <span className="icon github">
+                            <a target="_blank" href="https://github.com/mnorthup2207">
+                                <i className="fab fa-github"></i>
+                            </a>
+                        </span>
+                        <span className="icon linked">
+                            <a target="_blank" href="https://www.linkedin.com/in/mccabe-northup-2b76b885/">
+                                <i className="fab fa-linkedin-in"></i>
+                            </a>
+                        </span>
+                        <span className="icon resume">
+                            <a target="_blank" href={PDF}>
+                                <i className="fas fa-file-pdf"></i>
+                            </a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
 export default About;
